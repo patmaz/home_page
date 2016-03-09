8f385193a97888aa1409298c049e7e0302ebb826
patmaz <patrykiewicz@gmail.com>
2016-03-09 15:43:42 +0100

    fold menu

--- js-work/client.js
+++ js-work/client.js
@@ -1,38 +1,68 @@
 (function ($) {
     $(document).ready(function () {
-        var height = $(window).height();
         
+        //refresh on resize
+        $(window).resize(function () {
+            setTimeout(
+                function () {
+                    window.location.reload();
+                },
+                1);
+        });
+
+        var height = $(window).height();
+
         //scroll magic
         var controller = new ScrollMagic.Controller();
         // build scene
-        var scene = new ScrollMagic.Scene({triggerHook: "onCenter", duration: height/4})
-                        .setPin("#logo")
-                        .addTo(controller);
-        
-        $(window).scroll(function(){
-            // menu appear
+        var scene = new ScrollMagic.Scene({
+                triggerHook: "onCenter",
+                duration: height / 4
+            })
+            .setPin("#logo")
+            .addTo(controller);
+
+        $(window).scroll(function () {
+            // menu icon appear
             var scroll = $(window).scrollTop();
             var ratio = scroll / height;
-            
-            if(scroll >= (height - 100)){
-                $("#navbar").addClass("appear");
+
+            if (scroll >= (height - 100)) {
+                $("#menu-icon").addClass("appear");
             } else {
-                $("#navbar").removeClass("appear");
+                $("#menu-icon").removeClass("appear");
             }
             // down-arrwo disappear
-            if(scroll > 50){
+            if (scroll > 50) {
                 $("#down-arrow").addClass("hide");
             } else {
                 $("#down-arrow").removeClass("hide");
             }
-            
+
         });
-        
+
         //smooth scroll
-        $("#down-arrow").click(function(e){
-            $(window).scrollTo($("#main"), 800); 
+        $("#down-arrow").click(function (e) {
+            $(window).scrollTo($("#main"), 800);
         });
-        
+
+        $(".links").on("click", ".links-item", function (e) {
+            $(window).scrollTo($("#main"), 800);
+            $("#navbar").fadeOut(200);
+        });
+
+        //close navbar
+        $("#wrapper").on("click", "#menu-icon", function (e) {
+            $("#navbar").slideToggle(100);
+        });
+
+        $("#wrapper").on("click", "#close-icon", function (e) {
+            $("#navbar").slideToggle(100);
+        });
+
+        //navbar full height
+        $("#navbar").css("height", height);
+
     })
 })(jQuery);
 
@@ -78,11 +108,11 @@ app.controller("singlePage", ["$scope", "$http", "$sce", "$routeParams", functio
     };
 
     $http({
-    url: 'api/get_page/',
-    method: 'GET',
-    params: {
-        slug: $routeParams.id
-    }
+        url: 'api/get_page/',
+        method: 'GET',
+        params: {
+            slug: $routeParams.id
+        }
     }).success(function (res) {
         $scope.page = res.page;
         console.log("______");
--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,12 @@
+<div id="menu-icon">
+    <span class="genericon genericon-menu"></span>
+</div>
 <nav id="navbar" ng-controller="mainViewController">
     <ul class="links">
-        <li class="links-item"><a href="#/">portfolio</a></li>
-        <li class="links-item" ng-repeat="page in pages"><a ng-href="#/page/{{ page.slug }}">{{ page.title }}</a> </li>
+        <li><a class="links-item" href="#/">portfolio</a></li>
+        <li ng-repeat="page in pages"><a class="links-item" ng-href="#/page/{{ page.slug }}">{{ page.title }}</a> </li>
+        <li class="links-item"><span class="genericon genericon-github font-size-5"></span></li>
+        <li class="links-item"><span class="genericon genericon-linkedin-alt font-size-5"></span></li>
     </ul>
-    
+    <span id="close-icon" class="genericon genericon-close-alt font-size-5 center-down"></span>
 </nav>
